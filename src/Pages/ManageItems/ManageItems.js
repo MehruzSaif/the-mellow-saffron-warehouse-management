08d53a16e46e9025e703397db5d630c3b83cdcc6
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-06 02:53:03 +0600

    delete button added on manage component

--- src/Pages/ManageItems/ManageItems.js
+++ src/Pages/ManageItems/ManageItems.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import useItems from '../Hook/useItems';
 
 const ManageItems = () => {
+    const [items] = useItems();
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you sure about delete this product?');
+        if(proceed) {
+
+        }
+    }
+ 
     return (
-        <div>
+        <div className='w-50 mx-auto'>
             <h2>Manage Items</h2>
+            {
+                items.map(item => <div key={item._id}>
+                    
+                    <h5>{item.name} <button onClick={() => handleDelete(item._id)}>x</button></h5>
+                
+                </div>)       
+            }
         </div>
     );
 };
