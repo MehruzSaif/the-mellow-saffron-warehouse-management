50a980ac941f5809f307fe763e2c02d690d8d2fa
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-08 23:17:23 +0600

    about component modified

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -16,7 +16,7 @@ const About = () => {
                                     </h2>
 
                                     <h5 className='my-3'>
-                                        <b>We've been selling domestic and imported Spices and herbs in Bangladesh since 2014. Pran, Radhuni, BD-Food, Kishwan, ACI are among the 100+ brands available online and in our physical shop. Saffron, barberry, star alice etc are among the premium items names we carry. We've gained a significant competence in the senses of taste of spices throughout the time. In the next years, we plan to leverage technology to help spices choosing to make it simpler for our customers to choose the appropriate taste for them. Our staff members are agricultural and horticultural professionals that assist in selecting the highest quality items from throughout the country.</b>
+                                        <b>We've been selling domestic and imported Spices and herbs in Bangladesh since 2014. Pran, Radhuni, BD-Food, Kishwan, ACI are among the 100+ brands available online and in our physical shop. Saffron, barberry, star Anise etc are among the premium items names we carry. We've gained a significant competence in the senses of taste of spices throughout the time. In the next years, we plan to leverage technology to help spices choosing to make it simpler for our customers to choose the appropriate taste for them. Our staff members are agricultural and horticultural professionals that assist in selecting the highest quality items from throughout the country.</b>
                                     </h5>
 
                                 </div>
